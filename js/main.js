f3bebdae4357546afe1195c49d5d6ea7fff9f05e
DaDoDev7 <dario.plastina@gmail.com>
2023-01-25 16:06:53 +0100

    fifthpush

--- js/main.js
+++ js/main.js
@@ -1,24 +1,40 @@
-document.getElementById("top-button").addEventListener("click", function(){
-    window.scroll({
-        top: document.body.scrollHeight,
-        behavior:"smooth"
-    });
-});
+var halfWindow = window.innerHeight / 2;
+var navigation = document.querySelector(".top-button");
 
+window.addEventListener("scroll", function(){
+  var scrolled = window.scrollY;
+  if(scrolled >= halfWindow){
+      navigation.classList.remove('hide');
+  } else {
+      navigation.classList.add('hide');
+  }
+  
+})
 
-var swiper = new Swiper('.swiper', {
-  spaceBetween: 30,
-  centeredSlides: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
+// var topButton = document.querySelector("#return-to-top");
+// topButton.addEventListener("click", function() {
+// var top = document.querySelector("#return-to-top").getBoundingClientRect().top;
+// window.scroll({
+//     top: top, 
+//     left: 0, 
+//     behavior: 'smooth' 
+//   });
+// });
+
+var swiper = new Swiper(".mySwiper", {
+    
+    centeredSlides: false,
+    slidesPerView: 3,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
